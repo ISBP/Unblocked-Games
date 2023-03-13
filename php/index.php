547d3e41e813a17490bb7ecf121c97b3963e8c96
Bob-the-Bobothy <126205683+Bob-the-Bobothy@users.noreply.github.com>
2023-03-13 11:19:16 -0700

    fixed css

--- php/index.php
+++ php/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Turlock Rock and Gem Show</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/css/style.css">
       <link href="/media/rock_icon.png" rel="icon" />
 </head>
 <body>
