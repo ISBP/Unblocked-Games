dfd0a6cda862a40b087612688c207db8e77e6e94
PBSI <125078014+ISBP@users.noreply.github.com>
2023-03-28 20:33:12 +0000

    Added Slope

---
+++ games.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inject0r Games</title>
+    <link rel="stylesheet" href="style.css">
+      <link href="/media/rock_icon.png" rel="icon" />
+</head>
+<body>
+    <header>
+        <h1><a href="games.php">Welcome to our unblocked games!</a></h1>
+    </header>
+<main class="footer">
+    <h1><a href="geometry-dash.php">Geometry Dash</a></h1>
+    <a href="/geometry-dash.php">
+  <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/35/Geometry_Dash_Logo.PNG/250px-Geometry_Dash_Logo.PNG" alt="2D Runner" width="640" height="480">
+</a>
+  
+  <h1>Slope</h1>
+    <a href="/slope.php">
+  <img src="https://play-static.unity.com/20210320/p/images/a67259db-b315-42d2-bb09-283c0111e7bb_2D_Platformer_Screenshot.png" alt="2D Runner" width="640" height="480">
+</a>
+  
+    
+      
+      
+       
+</main>
+
+<footer class="footer">
+    <p>&copy; 2023 Math Help</p>
+</footer>
+</body>
+</html>
---
+++ slope.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inject0r Games</title>
+    <link rel="stylesheet" href="style.css">
+      <link href="/media/rock_icon.png" rel="icon" />
+</head>
+<body>
+    <header>
+        <h1><a href="games.php">⬅</a></h1>
+    </header>
+<iframe src="https://slope-game.cgolden15.repl.co" allowtransparency="true" width="1200" height="700" frameborder="0" scrolling="no" allowfullscreen></iframe>
+      
+      
+       
+</main>
+
+<footer class="footer">
+    <p>&copy; 2023 Math Help</p>
+</footer>
+</body>
+</html>
